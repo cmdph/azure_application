9362aad9b2cc333a8d73e6d7b9dc7f21c14c141c
chawki <chawkiaminou@gmail.com>
2022-06-18 17:17:25 +0200

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+import flask
+import json
+from flask import Flask, request, render_template
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
